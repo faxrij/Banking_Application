e8265ec1612caa5023edb41f2ecd1d77f21e8540
zeynepbektas <zeynepbektas>
2023-06-11 16:14:54 +0300

    guideline added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,6 +3,10 @@ package Main;
 import Bank.Bank;
 import MenuHandler.Handler.MainMenuHandler;
 
+
+// there is GUIDELINE.txt to make it easy to use
+// you can check it if you have a problem
+
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
